786b429fc57a74838d3845f521d1858deb7e1ea8
LetteraUnica <lorenzocav97@gmail.com>
2022-03-09 00:29:07 +0100

    create Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM eclipse-temurin:17-jdk
+
+EXPOSE 25565 25575
+
+RUN apt-get update -y
+
+RUN apt-get install git python3 -y
+
+VOLUME ["/data"]
+WORKDIR /data
+
+COPY clone.py .
---
+++ clone_server.py
@@ -0,0 +1,10 @@
+import os
+import subprocess
+
+dir="menzalib/"
+
+if not os.path.exists(dir):
+    subprocess.run(["git", "clone", 'https://github.com/LetteraUnica/menzalib.git'], shell=True)
+    print(f"Cloned {dir}")
+
+subprocess.run(["cd", dir], shell=True)
